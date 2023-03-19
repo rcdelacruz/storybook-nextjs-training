f523f39d39c5841e3339ace3a237a7b907b639d6
Bryan Matias <bmmatias@stratpoint.com>
2023-03-19 17:10:33 +0000

    feat: added responsive styling

--- components/navigation/footer/Footer.tsx
+++ components/navigation/footer/Footer.tsx
@@ -30,21 +30,43 @@ const Footer: React.FC<IFooter> = ({ sampleTextProp }) => {
       text: 'lorem',
       url: 'https://localhost:3000'
     },
+    {
+      text: 'lorem',
+      url: 'https://localhost:3000'
+    },
+    {
+      text: 'lorem',
+      url: 'https://localhost:3000'
+    },
+    {
+      text: 'lorem',
+      url: 'https://localhost:3000'
+    },
+    {
+      text: 'lorem',
+      url: 'https://localhost:3000'
+    },
+
   ]
   return (
     <footer
       className='w-full p-5 bg-slate-100 text-slate-500 text-center'
     >
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure, cumque?</p>
-      <div className="w-full ">
+      <p><a className='text-sky-400' href='#'>Lorem ipsum</a> - Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure, cumque?</p>
+      <div className="w-full max-md:grid grid-cols-4 my-4 flex justify-center">
         {
           footerLinks.map((footerLink, idx) => {
-            return (<Link className='px-2 text-sky-400'  key={idx} href={footerLink.url}>{footerLink.text}
+            return (<Link className='px-2 text-sky-400 first:list-none max-md:list-none' key={idx} href={footerLink.url}><li>{footerLink.text}</li>
             </Link>)
           })
         }
       </div>
-      <small>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda quibusdam magni delectus voluptate nulla dicta iusto ab animi possimus soluta?</small>
+      <p className="py-2">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos dolorem ullam adipisci explicabo accusamus sequi.
+      </p>
+      <p className="py-2">
+        <a className='text-sky-400' href='#'>Lorem ipsum</a> dolor sit amet consectetur, adipisicing elit.
+      </p>
     </footer>
   );
 };
--- components/navigation/header/Header.tsx
+++ components/navigation/header/Header.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
+import { HiOutlineMenu, HiOutlineSearch } from "react-icons/hi";
 
 export interface IHeader {
   sampleTextProp: string;
@@ -7,9 +8,12 @@ export interface IHeader {
 
 const Header: React.FC<IHeader> = ({ sampleTextProp }) => {
   return <header
-    className='w-full flex flex-row justify-between bg-slate-100' 
+    className='w-full flex flex-row justify-between bg-slate-100'
   >
-    <div className="flex space-x-5 m-5">
+    <div className="flex space-x-2 m-5">
+      <button className="hidden border p-2 px-4 sm:px-6 border-indigo-500 hover:bg-indigo-100 rounded text-indigo-500 max-md:block">
+        <HiOutlineMenu />
+      </button>
       <Link href="/">
         <Image
           src="images/devLogo.png"
@@ -19,24 +23,25 @@ const Header: React.FC<IHeader> = ({ sampleTextProp }) => {
           priority
         />
       </Link>
-      <div>
-        <form>
-          <input
-            type="text"
-            className="rounded-md border-2 w-5/6 sm:w-96 h-11 px-3"
-            placeholder="Search"
-          />
-             <button className="border ml-1 p-2 px-4 sm:px-6 border-indigo-500 hover:bg-indigo-100 rounded text-indigo-500">
-        Search
-      </button>
-        </form>
-      </div>
+      <form className="max-md:hidden">
+        <input
+          type="text"
+          className="rounded-md border-2 w-5/6 sm:w-96 h-11 px-3"
+          placeholder="Search"
+        />
+        <button className="border ml-1 p-2 px-4 sm:px-6 border-indigo-500 hover:bg-indigo-100 rounded text-indigo-500">
+          Search
+        </button>
+      </form>
     </div>
-    <div className="space-x-5 m-5">
-    <button className="border p-2 px-4 sm:px-6 border-indigo-500 hover:bg-indigo-100 rounded text-indigo-500">
+    <div className="space-x-2 m-5 flex align-center">
+      <button className="hidden border py-3 px-4 sm:px-6 bg-indigo-500 rounded text-white border-indigo-500 hover:bg-indigo-100 max-md:block">
+        <HiOutlineSearch/>
+      </button>
+      <button className="border p-2 px-4 sm:px-6 border-indigo-500 hover:bg-indigo-100 rounded text-indigo-500">
         Log in
       </button>
-      <button className="border-1 p-2 px-4 sm:px-6 bg-indigo-500 rounded text-white">
+      <button className="border-1 p-2 px-4 sm:px-6 bg-indigo-500 rounded text-white max-md:hidden">
         Create account
       </button>
     </div>
